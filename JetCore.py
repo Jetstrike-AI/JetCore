0723f5eb52b3decd897547cbdd9eab6668f6b462
Jetstrike-AI <denmaster664@gmail.com>
2022-03-10 19:38:22 +0300

    Alpha-0.0.1v

---
+++ JetCore.py
@@ -0,0 +1,27 @@
+# programming core with many features and utilities
+# alpha-0.0.1v
+
+
+def check_site(site):
+    from colorama import Fore, init
+    init()
+    import requests
+    s = requests.get(site)
+    r = s.status_code
+    if r == 200:
+        print(Fore.GREEN + "Site is available!")
+    else:
+        print(Fore.RED + "Site is not available!")
+
+
+def check_ping(ip):
+    import os
+    res = os.system("ping " + ip)
+    print(res)
+
+
+def thread(target, count):
+    import threading
+    for i in range(int(count)):
+        th = threading.Thread(target=target)
+        th.start()
